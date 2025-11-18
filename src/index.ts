99a98651745ab08b9fbfd0f489472f599661491c
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2025-11-19 01:58:09 +0530

    add cors support

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import readyRouter from "./routes/ready";
 import mealLogRouter from "./routes/mealLog";
+import cors from "cors";
 
 
 const app = express()
 app.use(express.json())
+app.use(cors())
+
 app.use("/ready", readyRouter)
 app.use("/mealLog", mealLogRouter)
 
