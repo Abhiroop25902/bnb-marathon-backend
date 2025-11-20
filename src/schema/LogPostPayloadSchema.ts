7bcb128dec3717faa429ebef80ad16efdb80ef2b
Abhiroop Mukherjee <abhiroop.m25902@gmail.com>
2025-11-20 23:30:39 +0530

    changes to LogPostPayloadSchema to support ui

--- src/schema/LogPostPayloadSchema.ts
+++ src/schema/LogPostPayloadSchema.ts
@@ -2,14 +2,14 @@ import {z} from 'zod'
 
 const LogPostPayloadSchema = z.object({
     imgUrl: z.string().optional(),
-    detectedFoods: z.array(z.string()),
-    estimatedMacros: z.object({
-        carbs_g: z.number(),
-        fat_g: z.number(),
-        protein_g: z.number(),
-    }),
+    // detectedFoods: z.array(z.string()),
+    // estimatedMacros: z.object({
+    //     carbs_g: z.number(),
+    //     fat_g: z.number(),
+    //     protein_g: z.number(),
+    // }),
     mealType: z.enum(["breakfast", "lunch", "snacks", "dinner", "other"]),
-    processed: z.boolean(),
+    // processed: z.boolean(),
     rawText: z.string(),
     symptoms: z.array(z.string()).optional(),
     createdAt: z.string()
